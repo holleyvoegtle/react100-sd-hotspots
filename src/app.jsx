dafe75a6d39635d7f1da4de20d8bb93f4d455081
Holley Voegtle <hvoegtle@gmail.com>
2021-10-27 11:36:45 -0700

    initial commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,44 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
+import TopSpot from './topspot';
 
-class App extends Component {
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+  componentWillMount() {
+    Axios 
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(response => response.data)
+      .then(topspots => this.setState({ topspots })); 
+   }
+
   render() {
     return (
-      <div className='App'></div>
+      <div className='App'>
+        <div className='container'>
+          <div className='jumbotron jumbotron-fluid padding-top-4'>
+          
+            <h1 className='display-4 text-center'>San Diego Top Spots</h1>
+            <p className='lead text-center'>A list of the top 30 places to see in San Diego, California.</p>
+            
+          </div>
+          { this.state.topspots.map(topspot => (
+		        <TopSpot
+			        key={topspot.id}
+			        name={topspot.name}
+			        description={topspot.description}
+			        location={topspot.location} 
+		        />
+	))
+}     
+        </div>  
+      </div>
+
     );
   }
 }
